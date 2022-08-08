5389aa7ce2195ddbabd60ec18a83547973ddc143
JannabiXoX <fyliu1519@gmail.com>
2022-08-08 15:29:53 +0800

    手工Maven修改为SpringBoot工程

---
+++ springboot-01-04-quickstart/src/main/java/com/lfy/controller/BookController.java
@@ -0,0 +1,16 @@
+package com.lfy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping
+    public String getById(){
+        System.out.printf("springboot is running...4");
+        return "springboot is running...4";
+    }
+}
