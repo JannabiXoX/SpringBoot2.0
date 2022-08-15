3f7cf6f7311aa149dbf7b4354e5640645f1d5d33
JannabiXoX <fyliu1519@gmail.com>
2022-08-15 11:30:15 +0800

    @ConfigurationProperties+松散绑定+计量单位绑定+数据校验

--- springboot_configuration/src/main/java/com/lfy/config/ServerConfig.java
+++ springboot_configuration/src/main/java/com/lfy/config/ServerConfig.java
@@ -2,13 +2,33 @@ package com.lfy.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.boot.convert.DurationUnit;
 import org.springframework.stereotype.Component;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 @Data
 //@Component
 @ConfigurationProperties(prefix = "servers")
+//2.开启对当前Bean的属性注入校验
+@Validated
 public class ServerConfig {
     private String ipAddress;
+    @Max(value = 8888,message = "最大值不能超过8888")
+    @Min(value = 100,message = "最小值不能低于100")
     private int port;
     private long timeout;
+
+    @DurationUnit(ChronoUnit.HOURS)
+    private Duration serverTimeOut;
+
+//    @DataSizeUnit(DataUnit.MEGABYTES)
+    private DataSize dataSize;
 }
--- springboot_configuration/src/test/java/com/lfy/SpringbootConfigurationApplicationTests.java
+++ springboot_configuration/src/test/java/com/lfy/SpringbootConfigurationApplicationTests.java
@@ -1,13 +1,19 @@
 package com.lfy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootConfigurationApplicationTests {
 
+    @Value("${servers.ipAddress}")
+    private String msg;
+
     @Test
     void contextLoads() {
+        System.out.println(msg);
     }
+
 
 }
